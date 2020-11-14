ae13e096621a2e4bf005ff591148f9181c24cac7
Shanejiang1 <jiangshanghang@gmail.com>
2020-11-14 17:38:42 +0800

    添加全局样式和全局变量

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -28,6 +28,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "~@/assets/style/helper.scss";
+
 nav {
   display: flex;
   box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
@@ -41,13 +43,15 @@ nav {
     justify-content: center;
     align-items: center;
     flex-direction: column;
-    .icon{
+
+    .icon {
       width: 32px;
       height: 32px;
     }
   }
-  > .item.selected{
-    color: #62b27a;
+
+  > .item.selected {
+    color: $color-highlight;
   }
 }
 
