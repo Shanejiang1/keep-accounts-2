46ac2911f0352451f527ddaf100362bb4d745454
Shanejiang1 <jiangshanghang@gmail.com>
2020-11-16 16:00:32 +0800

    添加 createdAt 字段

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -19,11 +19,14 @@ import Notes from '@/components/Money/Notes.vue';
 import Tags from '@/components/Money/Tags.vue';
 import {Component, Watch} from 'vue-property-decorator';
 
+const recordList: Record[] = JSON.parse(window.localStorage.getItem('recordList') || '[]');
+
 type Record = {
   tags: string[];
   notes: string;
   type: string;
-  amount: number;
+  amount: number; // 数据类型
+  createdAt?: Date; // 类 / 构造函数
 }
 
 @Component({
@@ -31,7 +34,7 @@ type Record = {
 })
 export default class Money extends Vue {
   tags = ['服饰', '餐饮', '交通', '住房'];
-  recordList: Record[] = [];
+  recordList: Record[] = recordList;
   record: Record = {tags: [], notes: '', type: '-', amount: 0};
 
   onUpdateTags(value: string[]) {
@@ -43,9 +46,9 @@ export default class Money extends Vue {
   }
 
   saveRecord() {
-    const record2 = JSON.parse(JSON.stringify(this.record));
+    const record2: Record = JSON.parse(JSON.stringify(this.record));
+    record2.createdAt = new Date();
     this.recordList.push(record2);
-    console.log(this.recordList);
   }
 
   @Watch('recordList')
